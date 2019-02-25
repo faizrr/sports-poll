1aaab89bc54107e4273875e4567dd6bfd3958ad2
Roman Faizullin <roman@faizullin.info>
2019-02-25 10:01:15 +0300

    Add loading state

--- packages/client/src/components/Button.tsx
+++ packages/client/src/components/Button.tsx
@@ -1,4 +1,4 @@
-import { css } from '@emotion/core'
+import { css, keyframes } from '@emotion/core'
 import styled from '@emotion/styled'
 import { lighten, darken } from 'polished'
 
@@ -6,10 +6,11 @@ import * as COLORS from '../constants/colors'
 
 type ButtonProps = {
   color: 'green' | 'white'
+  loading?: boolean
 }
 
 const greenColors = css`
-  background: ${COLORS.GREEN};
+  background-color: ${COLORS.GREEN};
   color: ${COLORS.WHITE};
 
   &:hover {
@@ -32,6 +33,30 @@ const whiteColors = css`
   }
 `
 
+const animation = keyframes`
+  0% {
+    background-position: 0 0;
+  }
+  100% {
+    background-position: 80px 0;
+  }
+`
+const loadingCss = css`
+  background-size: 80px 80px;
+  background-image: linear-gradient(
+    to right bottom,
+    rgba(255, 255, 255, 0.2) 25%,
+    rgba(0, 0, 0, 0) 25%,
+    rgba(0, 0, 0, 0) 50%,
+    rgba(255, 255, 255, 0.2) 50%,
+    rgba(255, 255, 255, 0.2) 75%,
+    rgba(0, 0, 0, 0) 75%,
+    rgba(0, 0, 0, 0)
+  );
+  animation: ${animation} 1s linear infinite;
+  pointer-events: none;
+`
+
 const Button = styled.button`
   height: 60px;
   border: 0px;
@@ -44,6 +69,8 @@ const Button = styled.button`
 
   ${(props: ButtonProps) => props.color === 'white' && whiteColors};
   ${(props: ButtonProps) => props.color === 'green' && greenColors};
+
+  ${(props: ButtonProps) => props.loading && loadingCss};
 
   &:focus {
     outline: 0;
